984afe8dcb132d5b8031ae7f4af694c52c40daef
pdvorak <peter.dvorak@abcrobotics.com>
2023-04-19 17:42:39 +0200

    Added K-Means clustering

--- Zadanie_4/main.py
+++ Zadanie_4/main.py
@@ -1,13 +1,17 @@
 import numpy as np
 import open3d as o3d
-import open3d.visualization
+from sklearn.cluster import KMeans
 
 
 def main():
-    show_by_image("./cow_and_lady.pcd")
+    # show_by_image(
+    #     "/home/pdvorak/school/pvso/PVSO_Zadania_Hasprun_Dvorak/Zadanie_4/cow_and_lady.pcd")
     # show_by_image("./output.pcd")
-    removed_outliers_pcd = remove_outliers('./cow_and_lady.pcd')
-    show_by_pcd(removed_outliers_pcd)
+    removed_outliers_pcd = remove_outliers(
+        '/home/pdvorak/school/pvso/PVSO_Zadania_Hasprun_Dvorak/Zadanie_4/cow_and_lady.pcd')
+    k_means(removed_outliers_pcd)
+
+    # show_by_pcd(removed_outliers_pcd)
 
 
 def show_by_image(image_name):
@@ -30,6 +34,30 @@ def remove_outliers(image):
     inlier_cloud = pcd.select_by_index(ind)
 
     return inlier_cloud
+
+
+def k_means(pcd):
+    # https://towardsdatascience.com/3d-point-cloud-clustering-tutorial-with-k-means-and-python-c870089f3af8
+    # Convert point cloud data to numpy array
+    xyz = np.asarray(pcd.points)
+
+    # n_clusters is the number of clusters we want.
+    # random_state makes the results reproducible and can be useful for debugging
+    kmeans = KMeans(n_clusters=20, random_state=1).fit(xyz)
+
+    # Get the cluster labels and the centroids
+    labels = kmeans.labels_
+    centroids = kmeans.cluster_centers_
+
+    # random color for each cluster
+    colors = np.random.rand(len(centroids), 3)
+
+    # Assign each point in the point cloud to a cluster based on the labels
+    pcd.colors = o3d.utility.Vector3dVector(np.zeros_like(xyz))
+    for i, label in enumerate(labels):
+        pcd.colors[i] = colors[label]
+    # Visualize the segmented point cloud
+    o3d.visualization.draw_geometries([pcd])
 
 
 main()
